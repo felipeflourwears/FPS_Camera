c7af149016b038827594e4d85a2eceabe238da62
felipeflourwears <A01274880@tec.mx>
2025-04-23 16:50:47 -0600

    Connect UI with Controller Camera

--- app/src/main/java/com/example/hypercamera/MainActivity.kt
+++ app/src/main/java/com/example/hypercamera/MainActivity.kt
@@ -2,6 +2,7 @@
 
 package com.example.hypercamera
 
+import CameraPreview
 import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -18,6 +19,8 @@ import androidx.core.content.ContextCompat
 import com.example.hypercamera.ui.theme.HyperCameraTheme
 
 import android.Manifest
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -25,7 +28,7 @@ import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.compose.foundation.layout.Box
-
+import androidx.compose.runtime.remember
 
 
 class MainActivity : ComponentActivity() {
@@ -39,6 +42,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             HyperCameraTheme {
                 val scaffoldState = rememberBottomSheetScaffoldState()
+                val controller = remember {
+                    LifecycleCameraController(applicationContext).apply{
+                        setEnabledUseCases(
+                            CameraController.IMAGE_CAPTURE or
+                                CameraController.VIDEO_CAPTURE
+                        )
+                    }
+                }
+
                 BottomSheetScaffold(
                     scaffoldState = scaffoldState,
                     sheetPeekHeight = 0.dp,
@@ -51,7 +63,11 @@ class MainActivity : ComponentActivity() {
                             .fillMaxSize()
                             .padding(padding)
                     ){
-
+                        CameraPreview(
+                            controller = controller,
+                            modifier = Modifier
+                                .fillMaxSize()
+                        )
                     }
 
                 }
